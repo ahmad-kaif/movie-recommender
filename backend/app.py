8c734d4f0f87d5c68a029e0b2feb3ee92f3f0b40
Ahmad Kaif <heyahmad611@gmail.com>
2025-02-24 23:21:30 +0530

    .

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 import pickle as pkl
 import pandas as pd
 import os
 
-app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
+app = Flask(__name__, static_folder='frontend/build', static_url_path='/')
+CORS(app)  # Enable CORS
 
-# Load the model and similarity matrix
+# Load model
 movies_dict = pkl.load(open('./model/movie_recommender_dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
 similarity = pkl.load(open('./model/similarity.pkl', 'rb'))
@@ -16,8 +16,7 @@ def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
     movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
-
-    recommended_movies = [movies.iloc[i[0]].title for i in movies_list]  # Return list of strings
+    recommended_movies = [movies.iloc[i[0]].title for i in movies_list]
     return recommended_movies
 
 @app.route('/recommend', methods=['POST'])
@@ -28,15 +27,20 @@ def get_recommendations():
     if not movie_name:
         return jsonify({"error": "No movie name provided"}), 400
 
-    recommended = recommend(movie_name)  # Should return a list of strings
+    recommended = recommend(movie_name)
+    return jsonify({"recommended_movies": recommended})
 
-    return jsonify({"recommended_movies": recommended})  # Ensure list of strings
+# Serve React Frontend
+@app.route('/')
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
 
-PORT = int(os.getenv("PORT", 5000))  # Default to 5000 if not set
+# Serve all frontend static files
+@app.route('/<path:path>')
+def serve_static(path):
+    return send_from_directory(app.static_folder, path)
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
+PORT = int(os.getenv("PORT", 5000))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=PORT)
